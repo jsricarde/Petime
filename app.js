49cced7966b3328c54e4b09695fd3b70f44a70d6
jsricarde <jsricarde@gmail.com>
2016-06-14 21:39:56 -0500

    WEB-01: Fixing Errors.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cfenv = require("cfenv")
 var appEnv = cfenv.getAppEnv();
 var app = express();
 var http = require('http').Server(app);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
  
@@ -15,10 +15,9 @@ app.use(express.static(path.join(__dirname, 'app')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
  
-/*http.createServer(app).listen(app.get('port'), function () {
-   console.log('myApp server listening on port ' + app.get('port'));
-});*/
 http.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.port);
 });
+
+module.exports = app;
